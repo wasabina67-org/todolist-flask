53aa78607192338f3746ee9fb389d78adfc6df66
wasabina67 <wasabina67@gmail.com>
2024-08-24 23:29:06 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, request
 
 from db import get_db
 from models import Todo
-from utils import status_success
+from utils import status_success, status_error
 from validators import TodoValidator
 
 app = Flask(__name__)
@@ -29,12 +29,15 @@ def api_todolist_get():
 
 @app.route("/api/todolist", methods=["POST"])
 def api_todolist_post():
-    request_dict = request.get_json()
-    tv = TodoValidator(name=request_dict["name"])
-    with get_db() as db:
-        db.add(Todo(name=tv.name))
-        db.commit()
-    return jsonify(status_success())
+    try:
+        request_dict = request.get_json()
+        tv = TodoValidator(name=request_dict["name"])
+        with get_db() as db:
+            db.add(Todo(name=tv.name))
+            db.commit()
+        return jsonify(status_success())
+    except Exception as ex:
+        return jsonify(status_error(ex))
 
 
 if __name__ == "__main__":
