2b33b1b422c67b835a7a8a8fceafc1cf58985729
wasabina67 <wasabina67@gmail.com>
2024-08-25 01:02:03 +0000

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,9 +13,9 @@ def client():
 def test_index(client):
     rv = client.get("/")
     assert rv.status_code == 200
-    assert b'<!DOCTYPE html>' in rv.data
+    assert b"<!DOCTYPE html>" in rv.data
 
 
 def test_api_todolist_get(client):
-    rv = client.get('/api/todolist')
+    rv = client.get("/api/todolist")
     assert rv.status_code == 200
