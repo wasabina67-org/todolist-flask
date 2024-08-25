e57daae87cbc86a6ffb107e88e43cea8dbb13514
wasabina67 <wasabina67@gmail.com>
2024-08-25 06:32:48 +0000

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,7 +9,7 @@ def test_index(client):
 
 
 def test_api_todolist_get(client, mocker):
-    mocker.patch(
+    mock = mocker.patch(
         "app.get_all_todos",
         return_value=[Todo(id=1, name="task-1", completed=False)],  # noqa
     )
@@ -17,6 +17,8 @@ def test_api_todolist_get(client, mocker):
     rv = client.get("/api/todolist")
     assert rv.status_code == 200
     assert rv.json == [{"id": 1, "name": "task-1", "completed": False}]
+
+    mock.assert_called()
 
 
 def test_api_todolist_post(client, mocker):
