513c9ed449df4855f8e307095866a36fe99e34f2
wasabina67 <wasabina67@gmail.com>
2024-08-25 00:57:06 +0000

    Update test_app.py

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -14,3 +14,8 @@ def test_index(client):
     rv = client.get("/")
     assert rv.status_code == 200
     assert b'<!DOCTYPE html>' in rv.data
+
+
+def test_api_todolist_get(client):
+    rv = client.get('/api/todolist')
+    assert rv.status_code == 200
