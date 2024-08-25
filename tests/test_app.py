e50a861746689df7b6e9f2ba054041e17b13b0f6
wasabina67 <wasabina67@gmail.com>
2024-08-25 03:40:12 +0000

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from models import Todo  # type: ignore
+from utils import status_error, status_success
 
 
 def test_index(client):
@@ -16,3 +17,29 @@ def test_api_todolist_get(client, mocker):
     rv = client.get("/api/todolist")
     assert rv.status_code == 200
     assert rv.json == [{"id": 1, "name": "task-1", "completed": False}]
+
+
+def test_api_todolist_post(client, mocker):
+    mocker.patch("app.insert_todo", return_value=None)
+
+    rv = client.post("/api/todolist", json={"name": "task-2"})
+    assert rv.status_code == 200
+    assert rv.json == status_success()
+
+
+def test_api_todolist_post_error(client, mocker):
+    mocker.patch("app.insert_todo", return_value=None)
+
+    rv = client.post("/api/todolist", json={"name": ""})
+    assert rv.status_code == 200
+    assert rv.json == status_error("Todo must be a non-empty string.")
+
+
+def test_api_todolist_post_error_2(client, mocker):
+    mocker.patch("app.insert_todo", return_value=None)
+
+    rv = client.post("/api/todolist", json={"name": "a" * 81})
+    assert rv.status_code == 200
+    assert rv.json == status_error(
+        "Todo must not be longer than 80 characters."
+    )
