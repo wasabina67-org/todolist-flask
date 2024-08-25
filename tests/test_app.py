d6cece0d03ebb66c92f33b5eb868c9c98f7102ba
wasabina67 <wasabina67@gmail.com>
2024-08-25 01:10:22 +0000

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,3 @@
-import pytest
-
-from app import app
-
-
-@pytest.fixture
-def client():
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
-
-
 def test_index(client):
     rv = client.get("/")
     assert rv.status_code == 200
